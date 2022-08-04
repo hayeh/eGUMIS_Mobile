6a0896dec4b429e6cc78d743fefe50d915670da2
hayeh <haris.zahari@pipeline-network.com>
2022-08-04 15:17:40 +0800

    egumis

---
+++ app/Http/Controllers/PelaporanController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePelaporanRequest;
+use App\Http\Requests\UpdatePelaporanRequest;
+use App\Models\Pelaporan;
+
+class PelaporanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePelaporanRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePelaporanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelaporan  $pelaporan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pelaporan $pelaporan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelaporan  $pelaporan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelaporan $pelaporan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePelaporanRequest  $request
+     * @param  \App\Models\Pelaporan  $pelaporan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePelaporanRequest $request, Pelaporan $pelaporan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelaporan  $pelaporan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pelaporan $pelaporan)
+    {
+        //
+    }
+}
